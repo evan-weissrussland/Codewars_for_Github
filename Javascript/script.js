50ef5190e60a017fe9393efa8de0e45db9e01ca8
evan-weissrussland <107429304+evan-weissrussland@users.noreply.github.com>
2023-05-02 21:40:01 +0300

    Create script.js

---
+++ Javascript/script.js
@@ -0,0 +1,13 @@
+"use strict"; /* включение сторого режима*/
+/*
+function digitize(n) {
+	let str = String(n);
+	let num = [];
+	for (let i = 0; i < str.length; i++) {
+		num.unshift(Number(str[i]));
+	}
+	return num;
+}
+
+console.log(digitize(123456789));
+*/
