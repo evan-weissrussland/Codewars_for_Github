e7afd46d9b7be316f2a023c30f8c0f360a2960a4
evan-weissrussland <107429304+evan-weissrussland@users.noreply.github.com>
2023-05-03 11:25:38 +0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -11,3 +11,13 @@ function digitize(n) {
 
 console.log(digitize(123456789));
 */
+/*
+let number = function(busStops) {
+	let numb = 0;
+	for (let i = 0; i < busStops.length; i++) {
+		numb += busStops[i][0] - busStops[i][1];
+	} 
+	return numb;
+}
+console.log(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]));
+*/
