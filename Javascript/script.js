1043cfc1c7c1643a805262c46524def60923cc21
evan-weissrussland <evanwodichka@gmail.com>
2023-05-18 22:12:28 +0300

    new task

--- Javascript/script.js
+++ Javascript/script.js
@@ -176,3 +176,13 @@ function DNAStrand(dna) {
 console.log(DNAStrand("AAAA"));
 */
 
+
+/*
+const summation = function (num) {
+   let sum = 0;
+   for (let i = 1; i <= num; i++) {
+     sum += i;
+   }
+   return sum;
+ }
+ */
