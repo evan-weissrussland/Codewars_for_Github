6b0aa9e5929ea655686bd46110a9bb9a52ae8e75
“evan-weissrussland” <evanwodichka@gmail.com>
2023-08-08 23:47:46 +0300

    new

--- Javascript/script.js
+++ Javascript/script.js
@@ -548,3 +548,10 @@ console.log(XO('zzoo'))
 
 console.log(XO("How can mirrors be real if our eyes aren't real"))*/
 //--------------------END task------------------
+//-----------Start task--------------------------
+/*function findShort(s) {
+    return s.split(' ').sort((a,b) => a.length - b.length)[0].length
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))*/
+//--------------------END task------------------
