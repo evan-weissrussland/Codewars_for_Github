4320302cb672fbab3a8fb42793b0b51674ebe749
“evan-weissrussland” <evanwodichka@gmail.com>
2023-09-04 20:09:17 +0300

    new

--- Javascript/script.js
+++ Javascript/script.js
@@ -663,27 +663,47 @@ const solution1 = (number) => {
 console.log(solution1(10))*/
 //--------------------END task------------------
 
-// //-----------Start task--------------------------
-// const uniqueInOrder = (data) => {
-//     if (typeof data === 'string') {
-//         data = data.split('')
-//     }
-//     console.log(data)
-//     const newArr = []
-//     outer: for (let i = 0; i < data.length; i++) {
-//         i === data.length - 1 && newArr.push(data[i])
-//         for (let j = i + 1; j < data.length; j++) {
-//             if (data[i] === data[j]) {
-//                 continue outer
-//             }
-//             newArr.push(data[i])
-//             continue outer
-//         }
-//     }
-//     return newArr
-// }
-//
-// console.log(uniqueInOrder('AAAABBBCCDAABBB'))
-// console.log(uniqueInOrder('ABBCcAD'))
-// console.log(uniqueInOrder([1,2,2,3,3]))
-// //--------------------END task------------------
+/*//-----------Start task--------------------------
+const uniqueInOrder = (data) => {
+    if (typeof data === 'string') {
+        data = data.split('')
+    }
+    console.log(data)
+    const newArr = []
+    outer: for (let i = 0; i < data.length; i++) {
+        i === data.length - 1 && newArr.push(data[i])
+        for (let j = i + 1; j < data.length; j++) {
+            if (data[i] === data[j]) {
+                continue outer
+            }
+            newArr.push(data[i])
+            continue outer
+        }
+    }
+    return newArr
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'))
+console.log(uniqueInOrder('ABBCcAD'))
+console.log(uniqueInOrder([1,2,2,3,3]))
+//--------------------END task------------------*/
+
+/*
+//-----------Start task--------------------------
+const comp = (a, b) => {
+    if (Array.isArray(a) && Array.isArray(b)) {
+        a = a.map(n => n * n)
+        a = a.sort((a,b)=>a-b)
+        b = b.sort((a,b)=>a-b)
+        for (let i = 0; i < a.length; i++) {
+            if (b[i] !== a[i]) {
+                return false
+            }
+        }
+        return true
+    }
+    return false
+}
+
+console.log(comp([9, 6, 10, 9, 0, 6, 5, 1, 0, 5, 1, 10, 1, 4, 3],[0, 25, 25, 9, 0, 36, 1, 81, 2, 1, 81, 16, 36, 100, 100] ))
+//--------------------END task------------------*/
